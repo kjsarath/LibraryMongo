fc6840ff44fc5a1ccbb43347b54c71217a2be609
Lenovo <Lenovo@LENOVO-PC>
2017-07-02 22:40:36 +0400

    Add project files.

---
+++ LibraryMongo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibraryMongo.MvcApplication" Language="C#" %>
---
+++ LibraryMongo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryMongo.Startup))]
+namespace LibraryMongo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
